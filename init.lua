0f586d24dd5359dd7951cc792c59c7a24c6c1208
Nico Auerswald <auersw.nico@gmail.com>
2026-05-31 15:57:14 +0200

    update init.lua

--- init.lua
+++ init.lua
@@ -110,7 +110,7 @@ do
   vim.o.number = true
   -- You can also add relative line numbers, to help with jumping.
   --  Experiment for yourself to see if you like it!
-  -- vim.o.relativenumber = true
+  vim.o.relativenumber = true
 
   -- Enable mouse mode, can be useful for resizing splits for example!
   vim.o.mouse = 'a'
@@ -391,10 +391,20 @@ do
     },
   }
 
+  vim.pack.add({
+    "https://github.com/ellisonleao/gruvbox.nvim"
+  })
+
+  require("gruvbox").setup {
+    styles = {
+      comments = { italic = false }, -- Disable italics in comments
+    },
+  }
+
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+  vim.cmd.colorscheme "tokyonight-night"
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
