e4f7d49e3267863f92b8b4fb93f782562151122c
Nico Auerswald <auersw.nico@gmail.com>
2026-06-01 09:43:37 +0200

    uppdate color scheme

--- init.lua
+++ init.lua
@@ -397,6 +397,8 @@ do
     },
   }
 
+  vim.pack.add { gh 'nyoom-engineering/oxocarbon.nvim' }
+
   vim.pack.add({
     "https://github.com/ellisonleao/gruvbox.nvim"
   })
@@ -417,9 +419,7 @@ do
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme "tokyonight-night"
-
-
+  vim.cmd.colorscheme "oxocarbon"
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
   require('todo-comments').setup { signs = false }
